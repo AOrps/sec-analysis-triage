e98a2bc29e81ab3f1eda006f5b29f51ed070b654
AOrps <aorbeandrews@gmail.com>
2023-09-21 23:26:02 -0400

    templating stuff
    Signed-off-by: AOrps <aorbeandrews@gmail.com>

--- core/main.go
+++ core/main.go
@@ -2,9 +2,9 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 	"strconv"
-	"log"
 	"text/template"
 )
 
@@ -12,25 +12,31 @@ const (
 	PORT = 9001
 )
 
-
 type NavLink struct {
 	Name string
 	Link string
 }
 
+// type TemplateSelector struct {
+// 	Name string
+// 	Opt string
+// }
+
 type Page struct {
-	Nav []NavLink
+	Nav            []NavLink
+	TemplateSelect string
+	Dynamic        bool
 }
 
 func Navbar() []NavLink {
 	return []NavLink{
 		{
-			Name: "Home",
+			Name: "/",
 			Link: "/",
 		},
 		{
 			Name: "Finding",
-			Link: "/finding",
+			Link: "/findings",
 		},
 		{
 			Name: "Filters",
@@ -42,71 +48,91 @@ func Navbar() []NavLink {
 		},
 		{
 			Name: "Tool Defects",
-			Link: "/tool_defects",
+			Link: "/tool-defects",
 		},
 		{
 			Name: "Wiki",
 			Link: "/wiki",
 		},
+		{
+			Name: "Settings",
+			Link: "/settings",
+		},
 	}
 }
 
-
 func rootHandle(w http.ResponseWriter, r *http.Request) {
-	tpl := template.Must(template.ParseGlob("templates/*.html"))
+	tpl := template.Must(template.ParseGlob("templates/*.htmx"))
 
 	page := Page{
-		Nav: Navbar(),
+		Nav:            Navbar(),
+		TemplateSelect: "root",
 	}
-	
-	tpl.ExecuteTemplate(w, "main",page)	
+
+	tpl.ExecuteTemplate(w, "main", page)
 
 }
 
 func findingsHandle(w http.ResponseWriter, r *http.Request) {
-	tpl := template.Must(template.ParseGlob("templates/*.html"))
+	tpl := template.Must(template.ParseGlob("templates/*.htmx"))
 
 	page := Page{
-		Nav: Navbar(),
+		Nav:            Navbar(),
+		TemplateSelect: "findings",
 	}
-	
-	tpl.ExecuteTemplate(w, "main",page)	
+
+	tpl.ExecuteTemplate(w, "main", page)
 }
 
 func filtersHandle(w http.ResponseWriter, r *http.Request) {
-	tpl := template.Must(template.ParseGlob("templates/*.html"))
+	tpl := template.Must(template.ParseGlob("templates/*.htmx"))
 	page := Page{
-		Nav: Navbar(),
+		Nav:            Navbar(),
+		TemplateSelect: "filters",
 	}
-	
-	tpl.ExecuteTemplate(w, "main",page)	
+
+	tpl.ExecuteTemplate(w, "main", page)
 }
 
 func casesHandle(w http.ResponseWriter, r *http.Request) {
-	tpl := template.Must(template.ParseGlob("templates/*.html"))
+	tpl := template.Must(template.ParseGlob("templates/*.htmx"))
 	page := Page{
-		Nav: Navbar(),
+		Nav:            Navbar(),
+		TemplateSelect: "cases",
 	}
-	
-	tpl.ExecuteTemplate(w, "main",page)	
+
+	tpl.ExecuteTemplate(w, "main", page)
 }
 
 func toolDefectsHandle(w http.ResponseWriter, r *http.Request) {
-	tpl := template.Must(template.ParseGlob("templates/*.html"))
+	tpl := template.Must(template.ParseGlob("templates/*.htmx"))
 	page := Page{
-		Nav: Navbar(),
+		Nav:            Navbar(),
+		TemplateSelect: "tooldefect",
 	}
-	
-	tpl.ExecuteTemplate(w, "main",page)	
+
+	tpl.ExecuteTemplate(w, "main", page)
 }
 
 func wikiHandle(w http.ResponseWriter, r *http.Request) {
-	tpl := template.Must(template.ParseGlob("templates/*.html"))
+	tpl := template.Must(template.ParseGlob("templates/*.htmx"))
 	page := Page{
-		Nav: Navbar(),
+		Nav:            Navbar(),
+		TemplateSelect: "wiki",
+	}
+
+	tpl.ExecuteTemplate(w, "main", page)
+}
+
+func settingsHandle(w http.ResponseWriter, r *http.Request) {
+	tpl := template.Must(template.ParseGlob("templates/*.htmx"))
+
+	page := Page{
+		Nav:            Navbar(),
+		TemplateSelect: "settings",
 	}
 	
-	tpl.ExecuteTemplate(w, "main",page)	
+	tpl.ExecuteTemplate(w, "main", page)
 }
 
 func graphqlHandle(w http.ResponseWriter, r *http.Request) {
@@ -122,19 +148,21 @@ func main() {
 	fs := http.FileServer(http.Dir("assets"))
 
 	// route files in ./assets to /assets/ in the html
-	mux.Handle("/assets/",http.StripPrefix("/assets/",fs))
+	mux.Handle("/assets/", http.StripPrefix("/assets/", fs))
 
 	// URL Routes
-	mux.HandleFunc("/",rootHandle)
-	mux.HandleFunc("/findings",findingsHandle)
-	mux.HandleFunc("/filters",filtersHandle)
-	mux.HandleFunc("/cases",casesHandle)
-	mux.HandleFunc("/tool-defects",toolDefectsHandle)
-	mux.HandleFunc("/wiki",wikiHandle)
-	mux.HandleFunc("/graphql",graphqlHandle)
-	
-	fmt.Println("server at http://localhost:"+port)
-	if http.ListenAndServe(":"+port,mux) != nil {
+	mux.HandleFunc("/", rootHandle)
+	mux.HandleFunc("/findings", findingsHandle)
+	mux.HandleFunc("/filters", filtersHandle)
+	mux.HandleFunc("/cases", casesHandle)
+	mux.HandleFunc("/tool-defects", toolDefectsHandle)
+	mux.HandleFunc("/wiki", wikiHandle)
+
+	mux.HandleFunc("/settings", settingsHandle)
+	mux.HandleFunc("/graphql", graphqlHandle)
+
+	fmt.Println("server at http://localhost:" + port)
+	if http.ListenAndServe(":"+port, mux) != nil {
 		log.Fatal("could not listen and serve")
 	}
 
